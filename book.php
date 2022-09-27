ee938a6225aa0c43e0b6bc2c0a41289b8a48cc69
EvertKa <93243148+EvertKa@users.noreply.github.com>
2022-09-27 11:38:00 +0300

    Add files via upload

--- book.php
+++ book.php
@@ -11,3 +11,24 @@ $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
 var_dump($book);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$book['title'];?></title>
+</head>
+<body>
+    <h1><?=$book['title'];?></h1>
+    <img src="<?=$book['cover_path']?>" alt="">
+    <p>Release date: <?=$book['release_date']?></p>
+    <p>Language: <?=$book['language']?></p>
+    <p>Description: <?=$book['summary']?></p>
+    <p>Price: <?=$book['price']?></p>
+    <p>Type: <?=$book['type']?></p>
+</body>
+</html>
